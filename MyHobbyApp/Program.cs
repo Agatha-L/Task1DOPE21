048b433d8fa647abc2cfd148e23cd9f54a401141
Agatha Laurent <jndaliagatha@gmail.com>
2021-05-23 16:21:01 -0700

    createdHoobbyApp

---
+++ MyHobbyApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyHobbyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
