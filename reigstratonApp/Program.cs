4f2b0df394b4ac80ee10be4c18349598223dba43
Agatha Laurent <jndaliagatha@gmail.com>
2021-05-26 12:04:21 -0700

    registrationApp updated

--- reigstratonApp/Program.cs
+++ reigstratonApp/Program.cs
@@ -6,7 +6,15 @@ namespace reigstratonApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("USER REGISTRATION");
+            Console.WriteLine("Account with UserName: ");
+            string name = Console.ReadLine();
+            //Console.WriteLine(name);
+
+            Console.WriteLine("Your Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}  {1} years", name, age);
+            
+         }
     }
 }
